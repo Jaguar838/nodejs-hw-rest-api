c6991e2a29b7a434ebe8be09cb3940a23eaf17c7
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-10-04 12:37:39 +0300

    start server
    GET /api/users
    {
        "status": "succes",
        "code": 200,
        "data": {
            "users": []
        }
    }

--- model/index.js
+++ model/index.js
@@ -1,53 +1,53 @@
-// const fs = require('fs/promises')
-// const contacts = require('./contacts.json')
-const crypto = required("crypto");
-const db = require;
-const listContacts = async () => {
+const crypto = require("crypto");
+const DB = require("./db");
+const db = new DB("users.json");
+
+const listUsers = async () => {
   return await db.read();
 };
 
-const getContactById = async (contactId) => {
-const users = await db.read();
-  const [user] = users.filter((user) => user.id === id);
-  return user
-};
+// const getUserId = async (UserId) => {
+// const users = await db.read();
+//   const [user] = users.filter((user) => user.id === id);
+//   return user
+// };
 
-const removeContact = async (contactId) => {
- const users = await db.read();
-  const index = users.findIndex((user) => user.id === id);
-  if (index !== -1) {
-    
-  }};
+// const removeUser = async (UserId) => {
+//  const users = await db.read();
+//   const index = users.findIndex((user) => user.id === id);
+//   if (index !== -1) {
 
-const addContact = async (body) => {
-  const users = await db.read();
-  const newUser = {
-    id: crypto.randomUUID(),
-    isVaccinated: false,
-    ...body,
-    // ...(body.isVaccinated ? {} : { isVaccinated: false }),
-    // используеться если прийдет или не прийдет поле с фронта
-  };
-  users.push(newUser);
-  await db.write(users);
-  return newUser;
-};
+//   }};
 
-const updateContact = async (contactId, body) => {
-  const users = await db.read();
-  const index = users.findIndex((user) => user.id === id);
-  if (index !== -1) {
-    const user = users[index]
-    users[index] = { ...user, ...}
-    return null
-  }
-  return 
-};
+// const addUser = async (body) => {
+//   const users = await db.read();
+//   const newUser = {
+//     id: crypto.randomUUID(),
+//     isVaccinated: false,
+//     ...body,
+//     // ...(body.isVaccinated ? {} : { isVaccinated: false }),
+//     // используеться если прийдет или не прийдет поле с фронта
+//   };
+//   users.push(newUser);
+//   await db.write(users);
+//   return newUser;
+// };
+
+// const updateUser = async (UserId, body) => {
+//   const users = await db.read();
+//   const index = users.findIndex((user) => user.id === id);
+//   if (index !== -1) {
+//     const user = users[index]
+//     users[index] = { ...user, ...}
+//     return null
+//   }
+//   return
+// };
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  listUsers,
+  // getUserId,
+  // removeUser,
+  // addUser,
+  // updateUser,
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,32 +1,45 @@
-const espress = require("express");
+const express = require("express");
 const router = express.Router();
-const Users = require("../../model")
-const { validateUser, validateID, validate?} = require('./validation')
 
-router.get("/:id", async (req, res, next) => {
-    try {
-        const user = await Users.getContactById(req.param.id)
-        if (user) {
-            
-        }
-    }
+const Users = require("../../model");
+// const patch = require("patch");
+// const { validateUser, validateID, validate?} = require('./validation')
+
+router.get("/", async (req, res, next) => {
+  try {
+    const users = await Users.listUsers();
+    res.json({ status: "succes", code: 200, data: { users } });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/", async (req, res, next) => {
-    try {
-        const user = await Users.addContact(req.body)
-        res.status(201)
-    }
-});
+// router.get("/:id", async (req, res, next) => {
+//     try {
+//         const user = await Users.getUserId(req.param.id)
+//         if (user) {
 
-router.delete('/:id', async (req, res, next) => {
-    res.json({'template message'})
-})
+//         }
+//     } catch (error) {
 
-router.patch('/:id/vaccinated', async (req, res, next) => {
-    try {
-        
-    }
-})
+//     }
+// });
 
-module.exports = router
+// router.post("/", async (req, res, next) => {
+//     try {
+//         const user = await Users.addContact(req.body)
+//         res.status(201)
+//     }
+// });
+
+// router.delete('/:id', async (req, res, next) => {
+//     res.json({'template message'})
+// })
+
+// router.patch('/:id/vaccinated', async (req, res, next) => {
+//     try {
+
+//     }
+// })
+
+module.exports = router;
