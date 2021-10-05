08e37dce0fa3e6d1f23f719c93ab86b82b892b93
BoberLob <75248620+Jaguar838@users.noreply.github.com>
2021-10-05 07:52:31 +0300

    validate field favorite
    http://joxi.ru/82Q5yYYfwnnzp2

--- bin/server.js
+++ bin/server.js
@@ -1,6 +1,8 @@
+const { required } = require("joi");
 const app = require("../app");
-const PORT = process.env.PORT || 3000;
+require("dotenv").config();
 
+const PORT = 3000 || process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running. Use our API on port: ${PORT}`);
 });
