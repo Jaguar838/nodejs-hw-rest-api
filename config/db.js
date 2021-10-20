be0a92683cb2db1d6002e7dcfca9fb7b3ec7ee30
Oleg Yamgurov <75248620+Jaguar838@users.noreply.github.com>
2021-10-20 22:54:04 +0300

    test mode: add test-db in mongoDB

--- config/db.js
+++ config/db.js
@@ -2,16 +2,23 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();
 
-const uri = process.env.URI_DB;
+let uri
+// test mode: add test-db in mongoDB 
+if (process.env.NODE_ENV == 'test') {
+  uri = process.env.URI_DB_TEST
+} else {
+  uri = process.env.URI_DB
+}
 
 const db = mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-mongoose.connection.on("connected", () => {
-  console.log("Mongoose connection to DB");
-});
+if (process.env.NODE_ENV == 'test') {
+  mongoose.connection.on('connected', () => {
+    console.log('Mongoose connection to DB')
+  })
 
 mongoose.connection.on("error", (err) => {
   console.log(`Mongoose connection error ${err.message}`);
